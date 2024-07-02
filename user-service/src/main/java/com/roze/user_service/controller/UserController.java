72dbcd91c45bdf0d43691881de03eaf130e2e8ad
ruta.rozene <rozene.ruta@gmail.com>
2024-07-02 15:03:21 +0300

    auth-service: added additional endpoints in controller and methods in service layer in user-service

--- user-service/src/main/java/com/roze/user_service/controller/UserController.java
+++ user-service/src/main/java/com/roze/user_service/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.roze.user_service.controller;
 
+import com.roze.user_service.dto.request.UserRequest;
 import com.roze.user_service.dto.response.UserResponse;
 import com.roze.user_service.service.UserService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Validated
@@ -19,11 +28,43 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostMapping()
+    public ResponseEntity<UserResponse> createUser(
+            @Valid @RequestBody UserRequest userRequest) {
+        UserResponse userResponse = userService.saveUser(userRequest);
+
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserResponse>> getAllUser() {
+        List<UserResponse> userList = userService.findAllUsers();
+
+        return ResponseEntity.ok(userList);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(
             @Min(1) @PathVariable Long id) {
         UserResponse userResponse = userService.findUserById(id);
 
         return ResponseEntity.ok(userResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUserById(
+            @Min(1) @PathVariable Long id,
+            @Valid @RequestBody UserRequest userRequest) {
+        UserResponse userResponse = userService.updateUserById(id, userRequest);
+
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById(
+            @Min(1) @PathVariable Long id) {
+        userService.deleteUserById(id);
+
+        return ResponseEntity.ok(String.format("User by ID %d was successfully deleted from data base", id));
     }
 }
--- user-service/src/main/java/com/roze/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/roze/user_service/service/impl/UserServiceImpl.java
@@ -1,13 +1,21 @@
 package com.roze.user_service.service.impl;
 
+import com.roze.user_service.dto.request.UserRequest;
 import com.roze.user_service.dto.response.UserResponse;
+import com.roze.user_service.enums.RoleName;
+import com.roze.user_service.exception.NoChangesMadeException;
+import com.roze.user_service.exception.UserAlreadyExist;
 import com.roze.user_service.mapper.UserMapper;
 import com.roze.user_service.persistance.UserRepository;
+import com.roze.user_service.persistance.model.RoleEntity;
 import com.roze.user_service.persistance.model.UserEntity;
+import com.roze.user_service.service.RoleService;
 import com.roze.user_service.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,6 +26,35 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserMapper userMapper;
 
+    @Autowired
+    private RoleService roleService;
+
+    @Override
+    public UserResponse saveUser(UserRequest userRequest) {
+
+        if (checkIfUserExistsByEmail(userRequest.getEmail())) {
+            throw new UserAlreadyExist(String.format("User with email %s already exists", userRequest.getEmail()));
+        }
+
+        UserEntity userToSave = userMapper.userRequestToUserEntity(userRequest);
+
+        userToSave.setName(sanitizeData(userRequest.getName()));
+        userToSave.setSurname(sanitizeData(userRequest.getSurname()));
+        userToSave.setRoles(roleService.getRoleEntitiesFormRoleNames(userRequest.getRoleNames()));
+
+        UserEntity savedUser = userRepository.save(userToSave);
+
+        return userMapper.userEntityToUserResponse(savedUser);
+    }
+
+    @Override
+    public List<UserResponse> findAllUsers() {
+
+        return userRepository.findAll().stream()
+                .map(userEntity -> userMapper.userEntityToUserResponse(userEntity))
+                .toList();
+    }
+
     @Override
     public UserResponse findUserById(Long id) {
 
@@ -25,5 +62,65 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new EntityNotFoundException("User not found by id: " + id));
 
         return userMapper.userEntityToUserResponse(userEntity);
+    }
+
+    public UserResponse updateUserById(Long id, UserRequest userRequest) {
+        UserEntity existingUser = getUserByIdOrThrow(id);
+
+        if (isUserDataEqual(existingUser, userRequest)) {
+            throw new NoChangesMadeException("User entry was not updated as no changes of entry were made.");
+        }
+
+        if (!existingUser.getEmail().equals(userRequest.getEmail()) && checkIfUserExistsByEmail(userRequest.getEmail())) {
+            throw new UserAlreadyExist(String.format("User with email %s already exists", userRequest.getEmail()));
+        }
+
+        updateUserData(existingUser, userRequest);
+        UserEntity savedUser = userRepository.save(existingUser);
+
+        return userMapper.userEntityToUserResponse(savedUser);
+    }
+
+    @Override
+    public void deleteUserById(Long id) {
+        getUserByIdOrThrow(id);
+        userRepository.deleteById(id);
+    }
+
+    private UserEntity getUserByIdOrThrow(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found by id: " + id));
+    }
+
+    private boolean checkIfUserExistsByEmail(String email) {
+        return userRepository.existsByEmailIgnoreCase(email);
+    }
+
+    private boolean isUserDataEqual(UserEntity existingUser, UserRequest userRequest) {
+        return existingUser.getName().equals(sanitizeData(userRequest.getName())) &&
+                existingUser.getSurname().equals(sanitizeData(userRequest.getSurname())) &&
+                existingUser.getEmail().equals(userRequest.getEmail()) &&
+                existingUser.getPhoneNumber().equals(userRequest.getPhoneNumber()) &&
+                isRolesEqual(existingUser.getRoles(), userRequest.getRoleNames());
+    }
+
+    private boolean isRolesEqual(List<RoleEntity> existingRoles, List<RoleName> requestedRoleNames) {
+        List<RoleName> existingRoleNames = existingRoles.stream()
+                .map(RoleEntity::getName)
+                .toList();
+
+        return existingRoleNames.equals(requestedRoleNames);
+    }
+
+    private void updateUserData(UserEntity existingUser, UserRequest userRequest) {
+        existingUser.setName(sanitizeData(userRequest.getName()));
+        existingUser.setSurname(sanitizeData(userRequest.getSurname()));
+        existingUser.setEmail(userRequest.getEmail());
+        existingUser.setPhoneNumber(existingUser.getPhoneNumber());
+        existingUser.setRoles(roleService.getRoleEntitiesFormRoleNames(userRequest.getRoleNames()));
+    }
+
+    private String sanitizeData(String data) {
+        return data.trim();
     }
 }
